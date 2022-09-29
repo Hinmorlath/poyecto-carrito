3e7afcc05486f14ef89ea1ccc7eb4a38f6ad69b6
Hinmorlath <josepolomanuel2@gmail.com>
2022-09-29 13:37:19 -0500

    Segundo intento

--- js/app.js
+++ js/app.js
@@ -6,14 +6,35 @@ const listaCursos = document.querySelector('#lista-cursos');
 
 cargarEventListener();
 function cargarEventListener(){
-    //
+    //Cuando agregas un curso presionando "Agregar al Carrito"
     listaCursos.addEventListener('click', agregarCurso);
 }
 
 // Funciones
 function agregarCurso(e){
     e.preventDefault();
+
     if(e.target.classList.contains('agregar-carrito')){
-        console.log(e.target);
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado)
     }
+}
+
+//Lee el contenido del HTML al que le dimos click y extrae la información del curso
+function leerDatosCurso(curso){
+    console.log(curso);
+
+    //Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio spam').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    //Agregar elementos del arreglo del carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+
+    console.log(articulosCarrito);
+    carritoHTML();
 }
